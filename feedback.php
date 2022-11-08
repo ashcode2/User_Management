100b4bac4abd14639e1912fb775a3111a55dd94f
ashcode2 <115816969+ashcode2@users.noreply.github.com>
2022-11-08 16:31:05 +0100

    Add files via upload

---
+++ feedback.php
@@ -0,0 +1,11 @@
+<?php
+ require_once 'assets/php/header.php';
+?>
+
+<h1><?= basename($_SERVER['PHP_SELF']); ?></h1>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
+<script type="text/javascript" 
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/js/all.min.js"></script>
+</body>
+</html>

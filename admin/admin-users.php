504a7e85d6cfaf6585fafbb051df5ab363ce5736
ashcode2 <115816969+ashcode2@users.noreply.github.com>
2022-11-16 12:00:27 +0100

    first commit

---
+++ admin/admin-users.php
@@ -0,0 +1,43 @@
+<?php
+require_once 'assets/php/admin-header.php';
+?>
+
+<div class="row">
+    <div class="col-lg-12">
+        <div class="card my-2 border-success">
+            <div class="card-header bg-success text-white">
+                <h4 class="m-0">Total Registered Users</h4>
+            </div>
+            <div class="card-body">
+                <div class="table-responsive" id="showAllUsers">
+                    <p class="text-center align-self-center lead">Please Wait...</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Footer Area -->
+</div>
+</div>
+</div>
+
+<script type="text/javascript">
+    $(document).ready(function(){
+
+        //Fetch All Users Ajax Request
+        fetchAllUsers();
+        function fetchAllUsers(){
+            $.ajax({
+                url: 'assets/php/admin-action.php',
+                method: 'post',
+                data: { action: 'fetchAllUsers' },
+                success:function(response){
+                    $("#showAllUsers").html(response);
+                }
+            });
+        }
+    });
+</script>
+</body>
+</html>

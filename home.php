418e0e643c2591fcd9da405c77cbea97361bda8d
ashcode2 <115816969+ashcode2@users.noreply.github.com>
2022-11-16 11:51:44 +0100

    first commit

--- home.php
+++ home.php
@@ -1,15 +1,250 @@
 <?php
- require_once 'assets/php/header.php';
+require_once 'assets/php/header.php';
 ?>
 
-<h1><?= basename($_SERVER['PHP_SELF']); ?></h1>
+<!-- partial -->
+<div class="main-panel">
+    <div class="content-wrapper">
+        <div class="row">
+            <div class="col-12 grid-margin stretch-card">
+                <div class="card corona-gradient-card">
+                    <div class="card-body py-0 px-0 px-sm-3">
+                        <div class="row align-items-center">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-xl-3 col-sm-6 grid-margin stretch-card">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-9">
+                                <div class="d-flex align-items-center align-self-start">
+                                    <h3 class="mb-0">$0</h3>
 
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
-<script type="text/javascript" 
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/js/all.min.js"></script>
+                                </div>
+                            </div>
+                            <div class="col-3">
+                                <div class="icon icon-box-success ">
+                                    <span class="mdi mdi-coin icon-item"></span>
+                                </div>
+                            </div>
+                        </div>
+                        <h6 class="text-muted font-weight-normal">Balance</h6>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-3 col-sm-6 grid-margin stretch-card">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-9">
+                                <div class="d-flex align-items-center align-self-start">
+                                    <h3 class="mb-0">$0</h3>
 
-</body>
+                                </div>
+                            </div>
+                            <div class="col-3">
+                                <div class="icon icon-box-success">
+                                    <span class="mdi mdi-arrow-bottom-left icon-item"></span>
+                                </div>
+                            </div>
+                        </div>
+                        <h6 class="text-muted font-weight-normal">Deposit</h6>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-3 col-sm-6 grid-margin stretch-card">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-9">
+                                <div class="d-flex align-items-center align-self-start">
+                                    <h3 class="mb-0">$0</h3>
+
+                                </div>
+                            </div>
+                            <div class="col-3">
+                                <div class="icon icon-box-danger">
+                                    <span class="mdi mdi-arrow-top-right icon-item"></span>
+                                </div>
+                            </div>
+                        </div>
+                        <h6 class="text-muted font-weight-normal">Withdraw</h6>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-3 col-sm-6 grid-margin stretch-card">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-9">
+                                <div class="d-flex align-items-center align-self-start">
+                                    <h3 class="mb-0">$0.3</h3>
+                                    <p class="text-success ml-2 mb-0 font-weight-medium">+3.5%</p>
+                                </div>
+                            </div>
+                            <div class="col-3">
+                                <div class="icon icon-box-success ">
+                                    <span class="mdi mdi-chart-bar"></span>
+                                </div>
+                            </div>
+                        </div>
+                        <h6 class="text-muted font-weight-normal">Bonus</h6>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4 grid-margin stretch-card">
+                <div class="card">
+                    <div class="card-body">
+                        <h4 class="card-title">Transaction History</h4>
+
+                        <div class="bg-gray-dark d-flex d-md-block d-xl-flex flex-row py-3 px-4 px-md-3 px-xl-4 rounded mt-3">
+                            <div class="text-md-center text-xl-left">
+                                <div class="icon icon-box-success ">
+                                    <span class="mdi mdi-currency-btc"></span>
+                                </div>
+                                <h6 class="mb-1">Transfer to Bitcoin</h6>
+                                <!--     <p class="text-muted mb-0">07 Jan 2019, 09:12AM</p> -->
+                            </div>
+                            <div class="align-self-center flex-grow text-right text-md-center text-xl-right py-md-2 py-xl-0">
+                                <h6 class="font-weight-bold mb-0">$0</h6>
+                            </div>
+                        </div>
+                        <div class="bg-gray-dark d-flex d-md-block d-xl-flex flex-row py-3 px-4 px-md-3 px-xl-4 rounded mt-3">
+                            <div class="text-md-center text-xl-left">
+                                <div class="icon icon-box-success ">
+                                    <span class="mdi mdi-currency-eth"></span>
+                                </div>
+                                <h6 class="mb-1">Transfer to Ethereum</h6>
+
+                            </div>
+                            <div class="align-self-center flex-grow text-right text-md-center text-xl-right py-md-2 py-xl-0">
+                                <h6 class="font-weight-bold mb-0">$0</h6>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-8 grid-margin stretch-card">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="d-flex flex-row justify-content-between">
+                            <h4 class="card-title mb-1">Referral</h4>
+                            <p class="text-muted mb-1">Status</p>
+                        </div>
+                        <div class="row">
+                            <div class="col-12">
+                                <div class="preview-list">
+                                    <div class="preview-item border-bottom">
+                                        <div class="preview-thumbnail">
+                                            <div class="preview-icon bg-primary">
+                                                <i class="mdi mdi-file-document"></i>
+                                            </div>
+                                        </div>
+                                        <div class="preview-item-content d-sm-flex flex-grow">
+                                            <div class="flex-grow">
+                                                <h6 class="preview-subject">Refer</h6>
+                                                <!--         <p class="text-muted mb-0">Broadcast web app mockup</p>
+                              </div>
+                              <div class="mr-auto text-sm-right pt-2 pt-sm-0">
+                                <p class="text-muted">15 minutes ago</p>
+                                <p class="text-muted mb-0">30 tasks, 5 issues </p>
+                              </div> -->
+                                            </div>
+                                        </div>
 
 
-</html>
+
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!--        -->
+
+            <div class="row">
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <h4 class="card-title">Visitors by Countries</h4>
+                            <div class="row">
+                                <div class="col-md-5">
+                                    <div class="table-responsive">
+                                        <table class="table">
+                                            <tbody>
+                                                <tr>
+                                                    <td>
+                                                        <i class="flag-icon flag-icon-us"></i>
+                                                    </td>
+                                                    <td>USA</td>
+                                                    <td class="text-right"> 1500 </td>
+                                                    <td class="text-right font-weight-medium"> 56.35% </td>
+                                                </tr>
+                                                <tr>
+                                                    <td>
+                                                        <i class="flag-icon flag-icon-de"></i>
+                                                    </td>
+                                                    <td>Germany</td>
+                                                    <td class="text-right"> 800 </td>
+                                                    <td class="text-right font-weight-medium"> 33.25% </td>
+                                                </tr>
+                                                <tr>
+                                                    <td>
+                                                        <i class="flag-icon flag-icon-au"></i>
+                                                    </td>
+                                                    <td>Australia</td>
+                                                    <td class="text-right"> 760 </td>
+                                                    <td class="text-right font-weight-medium"> 15.45% </td>
+                                                </tr>
+                                                <tr>
+                                                    <td>
+                                                        <i class="flag-icon flag-icon-gb"></i>
+                                                    </td>
+                                                    <td>United Kingdom</td>
+                                                    <td class="text-right"> 450 </td>
+                                                    <td class="text-right font-weight-medium"> 25.00% </td>
+                                                </tr>
+                                                <tr>
+                                                    <td>
+                                                        <i class="flag-icon flag-icon-ro"></i>
+                                                    </td>
+                                                    <td>Romania</td>
+                                                    <td class="text-right"> 620 </td>
+                                                    <td class="text-right font-weight-medium"> 10.25% </td>
+                                                </tr>
+                                                <tr>
+                                                    <td>
+                                                        <i class="flag-icon flag-icon-br"></i>
+                                                    </td>
+                                                    <td>Brasil</td>
+                                                    <td class="text-right"> 230 </td>
+                                                    <td class="text-right font-weight-medium"> 75.00% </td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                                <div class="col-md-7">
+                                    <div id="audience-map" class="vector-map"></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- content-wrapper ends -->
+
+
+
+        <?php
+        require_once 'assets/php/footer.php';
+        ?>
